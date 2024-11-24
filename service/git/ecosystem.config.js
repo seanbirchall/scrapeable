65925c37811967d96e417be35ac9a91aafb874d1
seanbirchall <156547477+seanbirchall@users.noreply.github.com>
2024-11-24 14:52:54 -0500

    UPDATE: config

--- service/git/ecosystem.config.js
+++ service/git/ecosystem.config.js
@@ -1,13 +1,21 @@
-// ecosystem.config.js.template
 module.exports = {
-    apps: [{
-      name: "git-github-webhook",
-      script: "index.js",
-      cwd: "/var/www/html/scrapeable/service/git",
-      env: {
-        NODE_ENV: "production"
-      },
-      watch: true,
-      max_memory_restart: '50M'
-    }]
-  };
+  apps: [{
+    name: "git-github-webhook",
+    script: "index.js",
+    cwd: "/var/www/html/scrapeable/service/git",
+    env: {
+      NODE_ENV: "production"
+    },
+    watch: true,
+    watch_delay: 1000,        // Delay between restarts
+    ignore_watch: [           // Ignore these files/folders
+      "node_modules",
+      "logs",
+      ".git",
+      "*.log"
+    ],
+    max_memory_restart: '200M',
+    exp_backoff_restart_delay: 100,
+    time: true
+  }]
+};
