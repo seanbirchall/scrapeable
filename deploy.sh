3a5b7d9a0e28077c5b4e2ca7d7ef4c8df4dc89b0
seanbirchall <156547477+seanbirchall@users.noreply.github.com>
2024-11-24 18:19:30 -0500

    UPDATE: use sean

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Ensure script runs as the sean user
+if [ "$(whoami)" != "sean" ]; then
+  sudo -u sean "$0" "$@"
+  exit
+fi
+
 DEPLOY_DIR="/var/www/html/scrapeable"
 BRANCH="main"
 cd "$DEPLOY_DIR"
